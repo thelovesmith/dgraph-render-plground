53077279e9bf95c1a17f9ea3de80aee6b34f4925
rderbier <rderbier@gmail.com>
2025-09-03 19:03:12 -0700

    test v25

--- dgraph/start.sh
+++ dgraph/start.sh
@@ -4,19 +4,20 @@
 if [ -n "$DGRAPH_TOKEN" ]; then
     export DGRAPH_ALPHA_SECURITY="whitelist=0.0.0.0/0;token=$DGRAPH_TOKEN"
     echo "Security token configured for Dgraph Alpha"
+    echo "DGRAPH_ALPHA_SECURITY=$DGRAPH_ALPHA_SECURITY"
 else
     export DGRAPH_ALPHA_SECURITY="whitelist=0.0.0.0/0"
     echo "Warning: DGRAPH_TOKEN not set, running without security"
 fi
 
 # Start Dgraph Zero in the background
-dgraph zero --config /dgraph/config/dgraph-config.yml &
+# dgraph zero --config /dgraph/config/dgraph-config.yml &
 
 # Wait a moment for Zero to start
-sleep 5
+# sleep 5
 
 # Start Dgraph Alpha
-dgraph alpha --config /dgraph/config/dgraph-config.yml &
+#dgraph alpha --config /dgraph/config/dgraph-config.yml &
 
 # Keep the container running indefinitely
 # This prevents the container from exiting even if dgraph processes are killed
