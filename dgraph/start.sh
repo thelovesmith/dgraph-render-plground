ac36f1e1b6185d04153bfb7bc0d6ab1ce39f577f
rderbier <rderbier@gmail.com>
2025-09-03 18:34:16 -0700

    rename dgraph

--- dgraph/start.sh
+++ dgraph/start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Set Dgraph Alpha security token from environment variable
+if [ -n "$DGRAPH_TOKEN" ]; then
+    export DGRAPH_ALPHA_SECURITY="whitelist: 0.0.0.0/0;token=$DGRAPH_TOKEN"
+    echo "Security token configured for Dgraph Alpha"
+else
+    export DGRAPH_ALPHA_SECURITY="whitelist: 0.0.0.0/0"
+    echo "Warning: DGRAPH_TOKEN not set, running without security"
+fi
+
 # Start Dgraph Zero in the background
 dgraph zero --config /dgraph/config/dgraph-config.yml &
 
